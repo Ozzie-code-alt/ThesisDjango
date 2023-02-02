6e6de8b00c85e4865bb3794aa9021b30f93461e2
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-02-02 16:14:52 +0800

    added more layers of filters

--- main.py
+++ main.py
@@ -20,5 +20,26 @@ get_image(hsv)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 get_image(gray, cmap= "gray")
 
-#blurreds...
+#blurrds...
+width, height = normal.shape[:2] #slice the flags
+print(width)
+print(height)
+if width > 500:
+    k=(50,50)
+elif width > 200 and width <=500:
+    k=(25,25)
+else:
+    k=(10,10)
+
+blur = cv2.blur(normal,k)
+get_image(blur)
+
+# binary Imaged
+v, binary = cv2.threshold(gray, 50,200, cv2.THRESH_BINARY)
+get_image(binary,cmap='gray')
+
+
+#inverted black and white
+inverted = cv2.bitwise_not(binary)
+get_image(inverted,cmap= 'gray')
 
