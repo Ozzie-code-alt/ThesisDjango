42dbd6770b3088e2affe9178b1b71c6b140b2a28
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-02-02 12:50:26 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread("images/pic.jpg")
+normal = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # converted to BGR format.
+
+
+def get_image(image, cmap =None):
+    plt.imshow(image, cmap = cmap)
+    plt.show()
+
+#Colorized
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+get_image(hsv)
+
+
+# grayscale
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+get_image(gray, cmap= "gray")
+
+#blurred
+
